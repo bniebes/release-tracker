3a95def27d70ffbdc49d24a4d326f6e7c79c144e
Brian Niebes <dev.BrianNiebes@gmail.com>
2024-08-20 23:43:50 +0200

    RT-6 - PROJ - Project skeleton

---
+++ src/main/java/de/iu/bniebes/ReleaseTrackerApplication.java
@@ -0,0 +1,11 @@
+package de.iu.bniebes;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ReleaseTrackerApplication {
+
+    public static void main(String[] args) {
+        log.atInfo().setMessage("Start ReleaseTrackerApplication").log();
+    }
+}
