d0c7b2da6edeee9cb7cb49570c06a602c4ffb841
Brian Niebes <dev.BrianNiebes@gmail.com>
2024-08-22 16:09:06 +0200

    RT-15 - FIX - Adjusted some values

--- src/main/java/de/iu/bniebes/configuration/DBConfiguration.java
+++ src/main/java/de/iu/bniebes/configuration/DBConfiguration.java
@@ -11,7 +11,7 @@ public record DBConfiguration(String jdbcUrl, String user, String password) {
     public static final String KEY_DB_JDBC_USER = COMMON_PREFIX + "url";
     public static final String KEY_DB_JDBC_PASSWORD_PATH = COMMON_PREFIX + "password";
 
-    public static final String DEFAULT_DB_JDBC_URL = "jdbc:postgresql://pgsql:5432/psd-batch";
+    public static final String DEFAULT_DB_JDBC_URL = "jdbc:postgresql://pgsql:5432/release_tracker";
     public static final String DEFAULT_DB_JDBC_USER = "release-tracker";
     public static final String DEFAULT_DB_JDBC_PASSWORD_PATH = GlobalConstants.Directories.SECRET + "/db_jdbc_password";
 
