897dc9cd5b593bc498bde5f82e79f75030385b18
Brian Niebes <dev.BrianNiebes@gmail.com>
2024-08-24 23:52:05 +0200

    RT-28 - IMPL - Result

---
+++ src/main/java/de/iu/bniebes/model/result/Result.java
@@ -0,0 +1,54 @@
+package de.iu.bniebes.model.result;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@EqualsAndHashCode
+public class Result<T> {
+
+    private final T value;
+
+    @Getter
+    private final boolean empty;
+
+    @Getter
+    private final boolean error;
+
+    private Result(final T value, boolean empty, boolean error) {
+        this.value = value;
+        this.empty = empty;
+        this.error = error;
+    }
+
+    public static <T> Result<T> of(final T value) {
+        return new Result<>(value, false, false);
+    }
+
+    public static <T> Result<T> ofEmpty() {
+        return new Result<>(null, true, false);
+    }
+
+    public static <T> Result<T> ofError() {
+        return new Result<>(null, false, true);
+    }
+
+    public boolean isPresent() {
+        return !empty && !error;
+    }
+
+    public boolean notPresent() {
+        return !isPresent();
+    }
+
+    public T get() throws IllegalStateException {
+        if (notPresent()) throw new IllegalStateException();
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        if (isPresent()) return "Result[%s]".formatted(value);
+        if (isEmpty()) return "Result.empty";
+        return "Result.error";
+    }
+}
---
+++ src/test/java/de/iu/bniebes/model/result/ResultTest.java
@@ -0,0 +1,36 @@
+package de.iu.bniebes.model.result;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ResultTest {
+
+    @Test
+    void result() {
+        final var testValue = "test";
+        final var dbResult = Result.of(testValue);
+        assertTrue(dbResult.isPresent());
+        assertFalse(dbResult.isEmpty());
+        assertFalse(dbResult.isError());
+        assertEquals(testValue, assertDoesNotThrow(dbResult::get));
+    }
+
+    @Test
+    void result_Empty() {
+        final var dbResult = Result.ofEmpty();
+        assertFalse(dbResult.isPresent());
+        assertTrue(dbResult.isEmpty());
+        assertFalse(dbResult.isError());
+        assertThrowsExactly(IllegalStateException.class, dbResult::get);
+    }
+
+    @Test
+    void result_Error() {
+        final var dbResult = Result.ofError();
+        assertFalse(dbResult.isPresent());
+        assertFalse(dbResult.isEmpty());
+        assertTrue(dbResult.isError());
+        assertThrowsExactly(IllegalStateException.class, dbResult::get);
+    }
+}
